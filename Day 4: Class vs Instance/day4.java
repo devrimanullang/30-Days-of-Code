09c8ad0bc3c7bc8cc83fadc806e20b19e8d10448
devrimanullang <devrimanullang@gmail.com>
2020-05-21 19:46:25 +0700

    day 4

---
+++ Day 4: Class vs Instance/day4.java
@@ -0,0 +1,28 @@
+public class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+  		if (initialAge < 0){
+            System.out.println("Age is not valid, setting age to 0.");
+            age = 0;
+        }else{
+            age = initialAge;
+        }
+        
+	}
+
+	public void amIOld() {
+  		String s = "";
+        if (age>=0 && age<13){
+            s = "You are young.";
+        } else if(age>=13 && age<18) {
+            s = "You are a teenager.";
+        }else{
+            s = "You are old.";
+        }
+        System.out.println(s);
+	}
+
+	public void yearPasses() {
+  		age ++;
+	}
